3d3d1024fb063d4f8c5809dbb1db53bbf0761937
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2025-11-03 10:35:43 -0800

    Updated the spinner logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import { RiseLoader } from "react-spinners";
 
 export default function App() {
+	const [weather, setWeather] = useState(null);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const city = "Los Angeles";
+
 	useEffect(() => {
 		const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
-		const city = "Los Angeles";
 		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-		axios
-			.get(url)
-			.then((response) => {
-				console.log(response.data);
-			})
-			.catch((error) => {
-				console.log("Error fectching weather data", error);
-			});
+		const fetchWeather = async () => {
+			try {
+				const response = await axios.get(url);
+				setLoading(true);
+				setWeather(response.data);
+			} catch (error) {
+				setError(error.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchWeather();
 	}, []);
 
+	if (loading) return <RiseLoader />;
+	if (error) return <p>{error}</p>;
+	if (!weather) return <p>No Weather Data!</p>;
+
 	return (
-		<>
-			<h1>Weather Dashboard</h1>
-		</>
+		<div>
+			<h1>{weather.name}</h1>
+		</div>
 	);
 }
