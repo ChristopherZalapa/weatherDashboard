ee870324d4ea1872b00faecd4f41ee456bc30e94
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2025-11-02 20:42:19 -0800

    Added axios and modified apikey

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import axios from "axios";
+
 export default function App() {
+	const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+	console.log(apiKey);
+
+	console.log(axios.isCancel("This is working"));
+
 	return (
 		<>
 			<h1>Weather Dashboard</h1>
