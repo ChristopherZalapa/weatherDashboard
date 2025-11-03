48e1d08cd9827e927ef1dbf9a5c7acc84c123546
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2025-11-02 21:08:05 -0800

    Basic axios api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import axios from "axios";
 
 export default function App() {
-	const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
-	console.log(apiKey);
+	useEffect(() => {
+		const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+		const city = "Los Angeles";
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-	console.log(axios.isCancel("This is working"));
+		axios
+			.get(url)
+			.then((response) => {
+				console.log(response.data);
+			})
+			.catch((error) => {
+				console.log("Error fectching weather data", error);
+			});
+	}, []);
 
 	return (
 		<>
