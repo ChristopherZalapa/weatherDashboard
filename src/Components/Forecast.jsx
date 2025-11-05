1cb0ca92aafc924d93df6d3b9aee0317fed3120a
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2025-11-05 10:51:20 -0800

    Added testing text to forecast.

--- src/Components/Forecast.jsx
+++ src/Components/Forecast.jsx
@@ -1,7 +1,31 @@
 export default function Forecast() {
 	return (
-		<div className='bg-white text-center dark:text-black \ max-w-2xl text-gray-900 shadow-md p-6 rounded-lg border-gray-300 mx-auto'>
-			5 Day Forecast
+		<div className='bg-white text-center dark:text-black \ max-w-2xl text-gray-900 shadow-md p-6 rounded-lg border-gray-300 mx-auto flex justify-between'>
+			<div className=''>
+				<h1>Day</h1>
+				<span>Icon</span>
+				<p>Temp</p>
+			</div>
+			<div className=''>
+				<h1>Day</h1>
+				<span>Icon</span>
+				<p>Temp</p>
+			</div>
+			<div className=''>
+				<h1>Day</h1>
+				<span>Icon</span>
+				<p>Temp</p>
+			</div>
+			<div className=''>
+				<h1>Day</h1>
+				<span>Icon</span>
+				<p>Temp</p>
+			</div>
+			<div className=''>
+				<h1>Day</h1>
+				<span>Icon</span>
+				<p>Temp</p>
+			</div>
 		</div>
 	);
 }
