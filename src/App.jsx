2aa8dae9a7bba6a4c7e49ccca6d15c5f171e9f73
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2025-11-05 10:37:10 -0800

    Updated temp toggle and dark light mode. Weather card is looking better.

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ import { RiseLoader } from "react-spinners";
 
 export default function App() {
 	const [weather, setWeather] = useState(null);
+	const [forecastWeather, setForecastWeather] = useState(null)
 	const [city, setCity] = useState("Los Angeles");
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(true);
 	const [darkMode, setDarkMode] = useState(false);
-	const [isFahrenheit, setIsFahrenheit] = useState(true);
+	const [isFahrenheit, setIsFahrenheit] = useState(false);
 
 	useEffect(() => {
 		if (city.trim() === "") {
@@ -47,12 +48,34 @@ export default function App() {
 
 	// WeatherCard Data
 	const displayCity = weather.name;
-	const displayTemp = weather.main.temp;
+
+	const displayTempCelsius = weather.main.temp;
+	const displayTemp = isFahrenheit
+		? temperatureFormat(convertToFahrenheit(displayTempCelsius))
+		: temperatureFormat(displayTempCelsius);
+
 	const displayHumidity = weather.main.humidity;
 	const displayIconCondition = weather.weather[0].main;
-	const displayDescription = weather.weather[0].description;
+
+	const displayDescriptionRaw = weather.weather?.[0]?.description ?? "";
+	const displayDescription = capitalizeWords(displayDescriptionRaw);
+
 	const displayWindSpeed = weather.wind.speed;
-	
+
+	function capitalizeWords(str = "") {
+		return str
+			.split(" ")
+			.map((w) => (w ? w[0].toUpperCase() + w.slice(1) : w))
+			.join(" ");
+	}
+
+	function temperatureFormat(temp) {
+		return Math.ceil(temp);
+	}
+
+	function convertToFahrenheit(celsius) {
+		return (celsius * 9) / 5 + 32;
+	}
 
 	function toggleDarkMode() {
 		setDarkMode(!darkMode);
@@ -67,30 +90,29 @@ export default function App() {
 	}
 
 	return (
-		<>
-			<div
-				className={`${
-					darkMode ? "dark" : ""
-				} bg-white dark:bg-zinc-800 min-h-screen w-full`}
-			>
-				<div className='container mx-auto px-4 py-8 max-w-4xl'>
-					<Headers
-						toggleDarkMode={toggleDarkMode}
-						toggleTemperature={toggleTemperature}
-						darkMode={darkMode}
-						isFahrenheit={isFahrenheit}
-					/>
-					<SearchBar cityChange={cityChange} />
-					<WeatherCard
-						displayCity={displayCity}
-						displayTemp={displayTemp}
-						displayHumidity={displayHumidity}
-						displayIconCondition={displayIconCondition}
-						displayDescription={displayDescription}
-						displayWindSpeed={displayWindSpeed}
-					/>
-				</div>
+		<div
+			className={`${
+				darkMode ? "dark" : ""
+			} bg-gray-100 dark:bg-zinc-800 min-h-screen w-full `}
+		>
+			<div className='container mx-auto px-4 py-8 max-w-4xl space-y-10'>
+				<Headers
+					toggleDarkMode={toggleDarkMode}
+					toggleTemperature={toggleTemperature}
+					darkMode={darkMode}
+					isFahrenheit={isFahrenheit}
+				/>
+				<SearchBar cityChange={cityChange} />
+				<WeatherCard
+					displayCity={displayCity}
+					displayTemp={displayTemp}
+					displayHumidity={displayHumidity}
+					displayIconCondition={displayIconCondition}
+					displayDescription={displayDescription}
+					isFahrenheit={isFahrenheit}
+					displayWindSpeed={displayWindSpeed}
+				/>
 			</div>
-		</>
+		</div>
 	);
 }
--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -9,11 +9,13 @@ import { VscPercentage } from "react-icons/vsc";
 export default function WeatherCard({
 	displayCity,
 	displayTemp,
+	isFahrenheit,
 	displayHumidity,
 	displayDescription,
 	displayIconCondition,
 	displayWindSpeed,
 }) {
+	// Weather Icons
 	const weatherIcons = {
 		Clear: <CiSun />,
 		Clouds: <CiCloudSun />,
@@ -28,20 +30,37 @@ export default function WeatherCard({
 	const icon = weatherIcons[displayIconCondition] || <CiCloudSun />;
 
 	return (
-		<>
-			<h1>{displayCity}</h1>
+		<div className='bg-white text-center dark:text-black \ max-w-2xl text-gray-900 shadow-md p-6 rounded-lg border-gray-300 mx-auto'>
+			<div className=''>
+				<h1 className=' font-bold text-3xl'>{displayCity}</h1>
+			</div>
 
-			<p>{displayDescription}</p>
-			<h2>{icon}</h2>
-			<p>
-				{displayHumidity}
-				<VscPercentage />
-			</p>
-			<p>
-				{displayTemp}
-				<RiCelsiusFill />
-			</p>
-			<p>{displayWindSpeed}</p>
-		</>
+			<h2 className=' mt-6 text-9xl self-center inline-flex items-center justify-center h-24 w-24'>
+				{icon}
+			</h2>
+			<div className='flex justify-center items-center space-x-3 mt-4'>
+				<p className=' font-medium text-3xl flex  justify-center'>
+					{displayTemp}
+
+					{isFahrenheit ? <RiFahrenheitFill /> : <RiCelsiusFill />}
+				</p>
+				<p className=''>{displayDescription}</p>
+			</div>
+
+			<div className='flex justify-between text-lg mt-6 px-20'>
+				<p className='inline-flex text-center justify-center items-center'>
+					Humidity &nbsp;
+					{displayHumidity}
+					<VscPercentage />
+				</p>
+				<p>
+					Wind &nbsp;
+					{displayWindSpeed}m/s
+				</p>
+			</div>
+		</div>
 	);
 }
+
+// word = 'hello'
+// word.toUpperCase()[0]
